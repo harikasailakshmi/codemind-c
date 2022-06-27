d2163b2b1efa34f2a33b84daaa1a05101220ed49
harikasailakshmi <97426068+harikasailakshmi@users.noreply.github.com>
2022-06-27 22:41:58 +0530

    #include <stdio.h> int main() {   int a,k,i,c=0,r,rev=0; 	scanf("%d",&a); 	int p=a;     k=a;     do     {         k=k/10;         ++c;     }     while (k != 0);    // printf("%d cc ",c);     for(i=0;i<c;i++)     {         r=a%10;     //    printf("%d rr ",r);         rev =rev*10+r;      //   printf("%d rev ",rev);         a=a/10;      //   printf("%d aa ",a);     }    // printf("%d",rev);    if(p==rev)    printf("True");    else    printf("False");
    }

--- Palindrome.c
+++ Palindrome.c
@@ -1,22 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int n,sum=0,r,k;
-scanf("%d",&n);
-k=n;
-while(n>0)
-{
-r=n%10;
-sum=sum*10+r;
-n=n/10;
-}
-if(k==sum)
-{
-printf("True");
-}
-else
-{
-printf("False");
-}
-return 0;
+  int a,k,i,c=0,r,rev=0;
+	scanf("%d",&a); 
+	int p=a;
+    k=a;
+    do 
+    {
+        k=k/10;
+        ++c; 
+    }
+    while (k != 0);
+   // printf("%d cc
+",c);
+    for(i=0;i<c;i++)  
+    {
+        r=a%10; 
+    //    printf("%d rr
+",r);
+        rev =rev*10+r;
+     //   printf("%d rev
+",rev);
+        a=a/10;
+     //   printf("%d aa
+",a);
+    }
+   // printf("%d",rev);
+   if(p==rev)
+   printf("True");
+   else
+   printf("False");
+   
 }
