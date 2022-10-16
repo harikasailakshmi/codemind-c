125bfe4482d52dbab6e68ef1372a1721dfa15608
harikasailakshmi <97426068+harikasailakshmi@users.noreply.github.com>
2022-10-16 14:34:20 +0530

    #include<stdio.h> int main() {     int n,i,r,s=0,c=0,temp,j,t;     scanf("%d",&n);     for(i=n+1;;i++)     {         temp=i;         s=0;         while(temp)         {             r=temp%10;             s=(s*10)+r;             temp/=10;         }         if(s==i)         {             t=s;             c=0;             for(j=1;j<=t;j++)             {                 if(t%j==0)                 {                     c++;                 }             }             if(c==2)             {                 printf("%d",s);                 break;             }         }     } }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,r,s=0,c=0,temp,j,t;
+    scanf("%d",&n);
+    for(i=n+1;;i++)
+    {
+        temp=i;
+        s=0;
+        while(temp)
+        {
+            r=temp%10;
+            s=(s*10)+r;
+            temp/=10;
+        }
+        if(s==i)
+        {
+            t=s;
+            c=0;
+            for(j=1;j<=t;j++)
+            {
+                if(t%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                printf("%d",s);
+                break;
+            }
+        }
+    }
+}
